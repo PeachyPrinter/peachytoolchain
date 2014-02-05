093de99c5ddb818f9223acc7f50b3aacda086757
james townley <james@townley.ca>
2014-02-05 09:58:24 -0600

    Started working on runnables in bin folder

---
+++ bin/calibrate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Starting Calibration"
+WORKING_DIRECTORY="${BASH_SOURCE%/*}"
+
+# Verifying Requirements
+python --version
+if [ $? != 0 ]; then
+    echo "Python not installed"
+    WILL_EXIT=true
+fi
+
+python -c 'import numpy'
+if [ $? != 0 ]; then
+    echo "numpy not installed"
+    WILL_EXIT=true
+fi
+
+python -c 'import pyaudio'
+if [ $? != 0 ]; then
+    echo "pyaudio not installed"
+    WILL_EXIT=true
+fi
+
+python -c 'import pyaudio'
+if [ $? != 0 ]; then
+    echo "PySide not installed"
+    WILL_EXIT=true
+fi
+
+if [ $WILL_EXIT ]; then
+    echo "Requirement's not satisfied."
+    echo "Have your read INSTALL.md"
+    echo "Exiting"
+    exit 1
+fi
+
+echo "Requirments met"
+
