c71b950072383ca01ce1abbd7bf25ce348855519
james townley <james@townley.ca>
2014-02-12 16:13:04 -0600

    Made the tests report a failing error code if tests are failling

--- test/test.py
+++ test/test.py
@@ -9,3 +9,5 @@ suite = loader.discover(os.path.dirname(__file__), pattern='*test.py')
 
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(suite)
+
+exit(len(result.errors) + len(result.failures))
