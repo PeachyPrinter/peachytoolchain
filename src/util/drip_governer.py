fdf70448e8034c2e8073ac55a5d977ab22b2dee6
james townley <james@townley.ca>
2014-03-10 20:59:05 -0600

    modified port

--- src/util/drip_governer.py
+++ src/util/drip_governer.py
@@ -3,14 +3,14 @@ import time
 import re
 
 class DripGoverner(object):
-    _VALID_PORT = re.compile("COM[0-9]")
+    # _VALID_PORT = re.compile("COM[0-9]")
     connection = None
     def __init__(self, port):
-        if self._VALID_PORT.match(port):
-            self.port = port
-        else:
-            raise ('%s is not a valid port try should be in the format "COM2"')
-        self.connection = serial.Serial("COM4", 9600)
+        # if self._VALID_PORT.match(port):
+        self.port = port
+        # else:
+            # raise ('%s is not a valid port try should be in the format "COM2"')
+        self.connection = serial.Serial(self.port, 9600)
 
     def start_dripping(self):
         self.connection.write("1")
